61d20b72e89735861317da206ae090530dd30726
lands8794 <99425133+lands8794@users.noreply.github.com>
2022-02-10 13:42:51 -0500

    Add files via upload
    P1 HW1 Python File

---
+++ P1HW1_LandSamuel.py
@@ -0,0 +1,10 @@
+num1 = int(input('enter your first number: '))
+num2 = int(input('enter your second number: '))
+
+add_nums = num1 + num2
+multiply_nums = num1 * num2
+
+print(num1)
+print(num2)
+print(add_nums)
+print(multiply_nums)
